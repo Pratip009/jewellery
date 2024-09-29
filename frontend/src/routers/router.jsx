62b976257515ccad9c22e42fc58619e072e2efcc
Pratip Kayal <pratipkayal1@gmail.com>
2024-09-29 12:19:13 +0530

    commit

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -5,7 +5,7 @@ import {
 import App from "../App";
 import Home from "../pages/home/Home";
 import CategoryPage from "../pages/Category/CategoryPage";
-import Search from "../pages/Search/Search";
+import Search from "../pages/search/Search.jsx";
 import ShopPage from "../pages/shop/ShopPage";
 import SingleProduct from "../pages/shop/productDetails/SingleProduct";
 const router = createBrowserRouter([
